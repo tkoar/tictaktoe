c5a3802099a693f859454f8ca8b27d3038708970
tkoar <terrancekoar@gmail.com>
2017-03-08 15:32:48 -0500

    updated functions and colors

--- tictactoe.js
+++ tictactoe.js
@@ -32,11 +32,33 @@ for (var i = 0; i < box.length; i++) {
       board[x][y] = player;
       testBoard[x][y] = true;
     }
+    checkForTicTacToe(board);
   }
 }
 
 console.log(board);
 
+function checkForTicTacToe(board) {
+  for (var i = 0; i < 3; i++) {
+    if (board[i][0] === "X" && board[i][1] === "X" && board[i][2] === "X") {
+      alert("congrats, player X! you won!");
+    } else if (board[0][i] === "X" && board[1][i] === "X" && board[2][i] === "X") {
+      alert("congrats, player X! you won!");
+    } else if (board[0][0] === "X" && board[1][1] === "X" && board[2][2] === "X") {
+      alert("congrats, player X! you won!");
+    } else if (board[0][2] === "X" && board[1][1] === "X" && board[2][0] === "X") {
+      alert("congrats, player X! you won!");
+    } else if (board[i][0] === "O" && board[i][1] === "O" && board[i][2] === "O") {
+      alert("congrats, player O! you won!");
+    } else if (board[0][i] === "O" && board[1][i] === "O" && board[2][i] === "O") {
+      alert("congrats, player O! you won!");
+    } else if (board[0][0] === "O" && board[1][1] === "O" && board[2][2] === "O") {
+      alert("congrats, player O! you won!");
+    } else if (board[0][2] === "O" && board[1][1] === "O" && board[2][0] === "O") {
+      alert("congrats, player O! you won!");
+    }
+  }
+}
 
 
 var button = document.getElementsByTagName('button');
