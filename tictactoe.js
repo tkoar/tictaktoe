8495c2e90a846f8a4cd164871dd7753dd99f341b
tkoar <terrancekoar@gmail.com>
2017-03-08 16:50:21 -0500

    updated colors and updated logic

--- tictactoe.js
+++ tictactoe.js
@@ -13,10 +13,15 @@ var testBoard = [
   [false, false, false]
 ];
 
-var player = "X";
-var p1 = "X";
-var p2 = "0";
+var x = (Math.floor((Math.random() * 10) + 1));
 
+if (x % 2 === 0) {
+    var y = "X";
+  } else {
+    y = "O";
+  }
+
+var player = y;
 
 var color = " red";
 
@@ -42,20 +47,28 @@ function checkForTicTacToe(board) {
   for (var i = 0; i < 3; i++) {
     if (board[i][0] === "X" && board[i][1] === "X" && board[i][2] === "X") {
       alert("congrats, player X! you won!");
+      break;
     } else if (board[0][i] === "X" && board[1][i] === "X" && board[2][i] === "X") {
       alert("congrats, player X! you won!");
+      break;
     } else if (board[0][0] === "X" && board[1][1] === "X" && board[2][2] === "X") {
       alert("congrats, player X! you won!");
+      break;
     } else if (board[0][2] === "X" && board[1][1] === "X" && board[2][0] === "X") {
       alert("congrats, player X! you won!");
+      break;
     } else if (board[i][0] === "O" && board[i][1] === "O" && board[i][2] === "O") {
       alert("congrats, player O! you won!");
+      break;
     } else if (board[0][i] === "O" && board[1][i] === "O" && board[2][i] === "O") {
       alert("congrats, player O! you won!");
+      break;
     } else if (board[0][0] === "O" && board[1][1] === "O" && board[2][2] === "O") {
       alert("congrats, player O! you won!");
+      break;
     } else if (board[0][2] === "O" && board[1][1] === "O" && board[2][0] === "O") {
       alert("congrats, player O! you won!");
+      break;
     }
   }
 }
